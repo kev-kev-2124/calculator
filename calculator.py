5badaa3b8ada7807d442b6d762d0e0ecf560c009
kev-kev-2124 <kev-kev-2124@users.noreply.github.com>
2016-03-13 20:28:06 -0700

    Update and rename README.md to calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# calculator
+
+def main():
+  operation = input(">>>Calculator: ")
+  
+  def num1():
+    input(int(operation))
+    
+  def num2():
+    input(int(operation))
+    
+  def op():
+    input('+' or '-' or '/' or '*'(operation))
+    
+  operation = ((num1) (op) (num2))
+  
+  return operation
