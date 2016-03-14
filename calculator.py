840d4ff2b990e85876a6168e8a6968dfaefae314
kev-kev-2124 <kev-kev-2124@users.noreply.github.com>
2016-03-14 14:48:50 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,14 +3,22 @@
 def calculator():
   operation = input(">>>Calculator: ")
   
+  # This part defines what num1 is
+  
   def num1():
     input(int(operation))
+    
+    # This part defines what num2 is
     
   def num2():
     input(int(operation))
     
+    # This part defines what op is
+    
   def op():
     input('+' or '-' or '/' or '*' or '**' or '%'(operation))
+    
+    # This part defines how you have to input your math problem
     
   operation = input((num1) (op) (num2))
   
